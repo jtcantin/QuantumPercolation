19aa7e9b01f61c674a8ea3043eaea5927e45a946
Joshua T Cantin <jtcantin@uwaterloo.ca>
2015-04-24 11:58:07 -0700

    Initial commit for Quantum Percolation module.

---
+++ src/quantumPercolation.cpp
@@ -0,0 +1,15 @@
+//
+//  quantumPercolation.cpp
+//  
+//
+//  Created by Joshua Tyler Cantin on 2015-04-24.
+//
+//
+
+#include "quantumPercolation.h"
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    cout << "Hello World." << endl;
+}
---
+++ src/quantumPercolation.h
@@ -0,0 +1,14 @@
+//
+//  quantumPercolation.h
+//  
+//
+//  Created by Joshua Tyler Cantin on 2015-04-24.
+//
+//
+
+#ifndef ____quantumPercolation__
+#define ____quantumPercolation__
+
+#include <iostream>
+
+#endif /* defined(____quantumPercolation__) */
