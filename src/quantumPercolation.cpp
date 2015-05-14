5a5093feee9ceecc1b5ec15163b51c84b54005ae
Joshua T Cantin <jtcantin@uwaterloo.ca>
2015-05-13 18:38:12 -0700

    dsyevr now running; identical results to dstevr for 10000 sites, but about 40 times slower (for NN hopping)

--- src/quantumPercolation.cpp
+++ src/quantumPercolation.cpp
@@ -4,7 +4,8 @@
 //
 //  Created by Joshua Tyler Cantin on 2015-04-24.
 //
-//
+// To run:
+// ./exec N E t randE_i[0(no) or 1(yes)] solver[0(dstevr), 1(dsyevr)]
 
 #include "quantumPercolation.h"
 
@@ -13,26 +14,40 @@ using namespace std;
 int main(int argc, char **argv) {
     cout << "Hello World." << endl;
     
+    int presInt = 10;
+    
     //Make Hamiltonian
     int N;
     double E;
     double t;
     int randTest;
+    int solver;
     
+    if (argc != 6) {
+        cout << "Not enough arguments!" << endl;
+        exit (EXIT_FAILURE);
+    }
     
     N = atoi(argv[1]);
     E = atof(argv[2]);
     t = atof(argv[3]);
     randTest = atoi(argv[4]);
+    solver = atoi(argv[5]);
     
     double** Hami;
+    double* Hami2;
     
     Hami = new double* [N];
+    Hami2 = new double [N*N];
     
     int i,j;
     
     for (i=0; i<N; i++) {
         Hami[i] = new double [N];
+        for (j=0; j<N; j++) {
+            Hami[i][j] = 0.0;
+            Hami2[N*i+j] = 0.0;
+        }
     }
     
     //Add in t and on-site energy
@@ -61,6 +76,23 @@ int main(int argc, char **argv) {
         }
     }
     
+    for (i=0; i<N; i++) {
+        for (j=0; j<N; j++) {
+            if (i==j) {
+                Hami2[N*i+j] = E;
+                if (randTest == 1) {
+                    Hami2[N*i+j] = LO + static_cast <double> (rand()) /( static_cast <double> (RAND_MAX/(HI-LO)));
+                }
+            }
+            else if (i-j == 1) {
+                Hami2[N*i+j] = t;
+            }
+            else if (i-j == -1) {
+                Hami2[N*i+j] = t;
+            }
+        }
+    }
+    
     
     
     // print out Hamiltonian
@@ -70,76 +102,161 @@ int main(int argc, char **argv) {
 //        }
 //        cout << endl;
 //    }
+//    
+//    for (i=0; i<N; i++) {
+//        for (j=0; j<N; j++) {
+//            cout << Hami2[N*i+j] ;
+//        }
+//        cout << endl;
+//    }
     
-    //Prepare Lapack dsterv call
-    lapack_int N_l;
-    double dummy = 0.0;
-    lapack_int dummy_int = 1;
+    switch (solver) {
+        case 0:
+        {
+            
+            //Prepare Lapack dsterv call
+            lapack_int N_l;
+            double dummy = 0.0;
+            lapack_int dummy_int = 1;
+            
+            //Output parameters
+            lapack_int numEval,isuppz;
+            double* evals = new double [N];
+            double** dummy_matrix = new double* [1];
+            dummy_matrix[0] = new double [1];
+            
+            lapack_int ldz = N;
+            int info;
+            
+        //    double reltol = 1E-7;
+        //    double minMag = ?;
+            double abstol;
+            
+            //Get the machine epsilon
+            abstol = LAPACKE_dlamch('E');
+            
+            double* diag;
+            double* subDiag;
+            
+            diag = new double [N];
+            subDiag = new double [N-1];
+            
+            //Assign diagonal elements
+        //    cout << "diag: ";
+            for (i=0; i<N; i++) {
+                diag[i] = Hami[i][i];
+        //        cout << diag[i];
+            }
+        //    cout << endl;
+            
+            //Assign subdiagonal elements
+        //    cout << "subDiag: ";
+            for (i=0; i<(N-1); i++) {
+                subDiag[i] = Hami[i][i+1];
+        //        cout << subDiag[i];
+            }
+        //    cout << endl;
+            
+            N_l = N;
+            
+            //I compared the below to the results from Numpy, and they agree to less than a relative error of 1E-14 for (N,E,t) = (10,1,2)
+            info = LAPACKE_dstevr(LAPACK_ROW_MAJOR,'N','A',N_l,diag, subDiag,dummy,dummy,dummy_int,dummy_int,abstol,&numEval,evals,*dummy_matrix,ldz,&isuppz);
+            
+            if (info == 0) {
+                cout << "LAPACKE_dstevr completed successfully." << endl;
+            }
+            else {
+                cout << "!!!LAPACKE_dstevr ERROR!!!" << endl;
+                cout << "dstevr info: " << info << endl;
+                exit (EXIT_FAILURE);
+            }
+            
+            cout << "NumEvals: " << numEval << endl;
+            cout << "Eigenvalues: " << endl;
+            cout << setprecision(presInt);
+            for (i=0; i<numEval; i++) {
+                cout << evals[i] << endl;
+            }
+            
+            delete[] diag;
+            delete[] subDiag;
+            delete [] evals;
+        }
+            break;
+            
+        case 1:
+        {
+            
+            // print out Hamiltonian
+//            for (i=0; i<N; i++) {
+//                for (j=0; j<N; j++) {
+//                    cout << Hami[i][j] ;
+//                }
+//                cout << endl;
+//            }
+            
+            
+            //Prepare Lapack dsyevr call
+            lapack_int N_l;
+//            double dummy = 0.0;
+            lapack_int dummy_int = 1;
+            
+            //Output parameters
+            lapack_int numEval,isuppz;
+            double* evals = new double [N];
+            double** dummy_matrix = new double* [1];
+            dummy_matrix[0] = new double [1];
+            
+            lapack_int ldz = N;
+            lapack_int lda = N;
+            int info;
+            
+            //    double reltol = 1E-7;
+            //    double minMag = ?;
+            double abstol;
+            
+            //Get the machine epsilon
+            abstol = LAPACKE_dlamch('E');
+            
+            
+            
+            N_l = N;
+            
+            //I compared the below to the results from Numpy, and they agree to less than a relative error of ??? for (N,E,t) = (10,1,2)
+            info = LAPACKE_dsyevr(LAPACK_ROW_MAJOR,'N','A','U',N_l,Hami2,lda,dummy_int,dummy_int,dummy_int,dummy_int,abstol, &numEval,evals,*dummy_matrix,ldz,&isuppz);
+            
+            if (info == 0) {
+                cout << "LAPACKE_dsyevr completed successfully." << endl;
+            }
+            else {
+                cout << "!!!LAPACKE_dsyevr ERROR!!!" << endl;
+                cout << "dstevr info: " << info << endl;
+                exit (EXIT_FAILURE);
+            }
+            
+            cout << "NumEvals: " << numEval << endl;
+            cout << "Eigenvalues: " << endl;
+            cout << setprecision(presInt);
+            for (i=0; i<numEval; i++) {
+                cout << evals[i] << endl;
+            }
+            delete [] evals;
+        }
+
+            
+            break;
+        default:
+            cout << "Solver not defined! No work done." << endl;
+            break;
+        }
     
-    //Output parameters
-    lapack_int numEval,isuppz;
-    double* evals = new double [N];
-    double** dummy_matrix = new double* [1];
-    dummy_matrix[0] = new double [1];
-    
-    lapack_int ldz = N;
-    int info;
-    
-//    double reltol = 1E-7;
-//    double minMag = ?;
-    double abstol;
-    
-    //Get the machine epsilon
-    abstol = LAPACKE_dlamch('E');
-    
-    double* diag;
-    double* subDiag;
-    
-    diag = new double [N];
-    subDiag = new double [N-1];
-    
-    //Assign diagonal elements
-//    cout << "diag: ";
-    for (i=0; i<N; i++) {
-        diag[i] = Hami[i][i];
-//        cout << diag[i];
-    }
-//    cout << endl;
-    
-    //Assign subdiagonal elements
-//    cout << "subDiag: ";
-    for (i=0; i<(N-1); i++) {
-        subDiag[i] = Hami[i][i+1];
-//        cout << subDiag[i];
-    }
-//    cout << endl;
-    
-    N_l = N;
-    
-    //I compared the below to the results from Numpy, and they agree to less than a relative error of 1E-14 for (N,E,t) = (10,1,2)
-    info = LAPACKE_dstevr(LAPACK_ROW_MAJOR,'N','A',N_l,diag, subDiag,dummy,dummy,dummy_int,dummy_int,abstol,&numEval,evals,*dummy_matrix,ldz,&isuppz);
-    
-    if (info == 0) {
-        cout << "LAPACKE_dstevr completed successfully." << endl;
-    }
-    else {
-        cout << "!!!LAPACKE_dstevr ERROR!!!" << endl;
-        cout << "dstevr info: " << info << endl;
-        exit (EXIT_FAILURE);
-    }
     
     
-    cout << "NumEvals: " << numEval << endl;
-    cout << "Eigenvalues: " << endl;
-    cout << setprecision(10);
-    for (i=0; i<numEval; i++) {
-        cout << evals[i] << endl;
-    }
     
     for (i=0; i<N; i++) {
         delete[] Hami[i];
     }
     delete[] Hami;
-    delete[] diag;
-    delete[] subDiag;
+    delete[] Hami2;
+    
 }
